0217444febccb53bf39883f0c82789f09701df1d
caroline.hjq@gmail.com <hanjq@kaolafm.com>
2016-12-20 19:15:03 +0800

    add the side of send message and update pom.xml

---
+++ rabbitmq/src/main/java/com/caroline/exe/Send.java
@@ -0,0 +1,44 @@
+package com.caroline.exe;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * User: Caroline.Han
+ * Date: 2016-12-20
+ * Time: 上午11:06
+ */
+public class Send {
+
+    //队列名称
+    private final static String QUEUE_NAME = "Hello";
+
+    public static void main(String[] argv) throws IOException, TimeoutException {
+
+        /**
+         * 创建连接 连接到RabbitMQ
+         */
+        ConnectionFactory factory = new ConnectionFactory();
+        //设置RabbitMQ所在主机ip或者主机名
+        factory.setHost("localhost");
+        //创建一个连接
+        Connection  connection = factory.newConnection();
+        //创建一个频道
+        Channel channel = connection.createChannel();
+        //指定一个队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        //发送的消息
+        String message = "Hello world";
+        //往队列中发出一条信息
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+        //关闭频道和连接
+        channel.close();
+        connection.close();
+    }
+}
