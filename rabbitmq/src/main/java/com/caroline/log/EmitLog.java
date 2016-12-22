0427ffa4f2642c57986abfbb56a3e46d36cf5448
caroline.hjq@gmail.com <hanjq@kaolafm.com>
2016-12-22 11:15:08 +0800

    add exchanges on the side of send

---
+++ rabbitmq/src/main/java/com/caroline/log/EmitLog.java
@@ -0,0 +1,35 @@
+package com.caroline.log;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * User: Caroline.Han
+ * Date: 2016-12-22
+ * Time: 上午11:05
+ */
+public class EmitLog {
+
+    private final static String EXCHANGE_NAME = "ex_log";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //创建连接和频道
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        //声明转发器和类型
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        String message = new Date() + " : log something";
+        channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
+
+        System.out.println(" [x] Sent '" + message + "'");
+        channel.close();
+        connection.close();
+    }
+}
